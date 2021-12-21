bc02f6d848afd6d6fcadc9973077fedff0695ffe
Admin <alfaruk1612@gmail.com>
2021-12-21 14:57:43 +0600

    local database

---
+++ lib/features/domain/usecases/login.dart
@@ -0,0 +1,22 @@
+
+import 'package:applications/core/error/text_field_info.dart';
+import 'package:applications/features/domain/repositories/applications_repository.dart';
+import 'package:applications/features/presentation/getx/controllers/login_controller.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class UserLogin {
+  void login(String userName, String password) {
+    final loginController = Get.find<LoginController>();
+    if (!TextFieldInfo().checkTextField(userName)) {
+      loginController.userNameRequired(true);
+      FocusScope.of(Get.context!).requestFocus(loginController.userNameFocusNode);
+    } else if (!TextFieldInfo().checkTextField(password)) {
+      loginController.passwordRequired(true);
+      FocusScope.of(Get.context!).requestFocus(loginController.passwordFocusNode);
+    } else {
+      ApplicationRepository().login(userName, password);
+    }
+  }
+
+}
